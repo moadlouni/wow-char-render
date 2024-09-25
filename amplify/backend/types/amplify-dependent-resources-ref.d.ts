fd92664aa6403367ab10cbdadcc80a3e8abe5a69
mo adlouni <moadlouni@yahoo.com>
2024-09-25 11:56:16 -0500

    aws back end services, graphQL, auth, and s3 bucket added.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "wowcharrender": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "wowcharrenderb2a8e6b9": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "wowcharrenders3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
